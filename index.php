7a54d4d44266a72b3cceeee5780bf2ac13a594b2
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-03-25 10:52:32 +0100

    first

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- http://www.cellbiol.com/bioinformatics_web_development/chapter-4-adding-a-dynamic-layer-introducing-the-php-programming-language/dynamic-web-pages-with-php-a-simple-yet-useful-example/ -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>My great web site</title>
+        <meta name="description" contents="Just a test website for learning html, css and php">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <nav id="main-navigation">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="page2.php">Page 2</a></li>
+                    <li><a href="page3.php">Page 3</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="main-contents">
+            This is page 1, the home page.
+        </div>
+        <footer>
+            Contact us at webmaster@mywebsite.com
+        </footer>
+    </body>
+</html>
